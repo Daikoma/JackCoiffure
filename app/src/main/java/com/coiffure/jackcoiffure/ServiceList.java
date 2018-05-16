c77c5ccd8d98e24cb5d4bdf6edc70c7ffce5abad
Valentin <v.morin59@gmail.com>
2018-05-16 10:24:24 +0200

    Rendre la liste de services clickable
    Quelques implémentations pour permettre de rendre la liste de service clickable.

--- app/src/main/java/com/coiffure/jackcoiffure/ServiceList.java
+++ app/src/main/java/com/coiffure/jackcoiffure/ServiceList.java
@@ -1,10 +1,27 @@
 package com.coiffure.jackcoiffure;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,29 +36,52 @@ import com.android.volley.toolbox.Volley;
 
 import com.coiffure.jackcoiffure.globalVariables.Global;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ServiceList extends AppCompatActivity {
 
-    private TextView service_list;
+    private ListView service_list;
     private RequestQueue mQueue;
+    private ArrayList<String> data;
+
+    private String items[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_list);
 
-        service_list = findViewById(R.id.service_list);
-        Button buttonParse = findViewById(R.id.button_parse);
-
         mQueue = Volley.newRequestQueue(this);
+        data = new ArrayList<>();
 
-        buttonParse.setOnClickListener(new View.OnClickListener() {
+        getServices();
+        
+        String zizi_string = "" + data.size();
+        
+        items = new String[] {zizi_string, "caca"};
+        /*items = new String[data.size()];
+        for (int i = 0; i < data.size(); i++) {
+            items[i] = data.get(i);
+        }*/
+
+
+        service_list = findViewById(R.id.service_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        service_list.setAdapter(adapter);
+
+        service_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v){
-                getServices();
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(ServiceList.this, data.get(position), Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
+
+
 
     public void getServices() {
 
@@ -63,7 +103,9 @@ public class ServiceList extends AppCompatActivity {
                                 String description = service.getString("description");
                                 int price = service.getInt("price");
 
-                                service_list.append(name + ", " + description + ", " +String.valueOf(price) + " \n\n");
+                                //service_list.append(name + ", " + description + ", " +String.valueOf(price) + " \n\n");
+                                //data.add(name + ", " + description + ", " +String.valueOf(price) + " \n\n");
+                                data.add(name);
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -78,6 +120,16 @@ public class ServiceList extends AppCompatActivity {
 
         mQueue.add(request);
     }
+
+
+    public void makeClickable() {
+
+    }
+
+
+    /*public void text_clicked(View view) {
+        Toast.makeText(getApplicationContext(), separated.get(0), Toast.LENGTH_SHORT).show();
+    }*/
 
 }
 
