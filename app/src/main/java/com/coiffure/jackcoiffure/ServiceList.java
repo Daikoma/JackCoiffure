146ba80553e50bbe0a257a5a599389fa48f8fa38
Valentin <v.morin59@gmail.com>
2018-05-16 12:26:07 +0200

    Quelques modifications

--- app/src/main/java/com/coiffure/jackcoiffure/ServiceList.java
+++ app/src/main/java/com/coiffure/jackcoiffure/ServiceList.java
@@ -1,26 +1,11 @@
 package com.coiffure.jackcoiffure;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -37,7 +22,6 @@ import com.android.volley.toolbox.Volley;
 import com.coiffure.jackcoiffure.globalVariables.Global;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class ServiceList extends AppCompatActivity {
@@ -53,34 +37,28 @@ public class ServiceList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_list);
 
+        service_list = findViewById(R.id.service_list);
+
         mQueue = Volley.newRequestQueue(this);
         data = new ArrayList<>();
 
         getServices();
-        
+
         String zizi_string = "" + data.size();
-        
         items = new String[] {zizi_string, "caca"};
-        /*items = new String[data.size()];
-        for (int i = 0; i < data.size(); i++) {
-            items[i] = data.get(i);
-        }*/
 
 
-        service_list = findViewById(R.id.service_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(ServiceList.this, android.R.layout.simple_list_item_1, data);
         service_list.setAdapter(adapter);
 
         service_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(ServiceList.this, data.get(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(ServiceList.this, "" +data.size(), Toast.LENGTH_SHORT).show();
             }
         });
 
-
     }
-
 
 
     public void getServices() {
@@ -91,6 +69,7 @@ public class ServiceList extends AppCompatActivity {
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,
                 new Response.Listener<JSONObject>() {
+
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
@@ -103,10 +82,9 @@ public class ServiceList extends AppCompatActivity {
                                 String description = service.getString("description");
                                 int price = service.getInt("price");
 
-                                //service_list.append(name + ", " + description + ", " +String.valueOf(price) + " \n\n");
-                                //data.add(name + ", " + description + ", " +String.valueOf(price) + " \n\n");
                                 data.add(name);
                             }
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -120,16 +98,6 @@ public class ServiceList extends AppCompatActivity {
 
         mQueue.add(request);
     }
-
-
-    public void makeClickable() {
-
-    }
-
-
-    /*public void text_clicked(View view) {
-        Toast.makeText(getApplicationContext(), separated.get(0), Toast.LENGTH_SHORT).show();
-    }*/
 
 }
 
